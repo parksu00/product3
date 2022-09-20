63ac92ce23919b2c74b24a184dc2ab80fa185d5a
ParkSJ00 <youi1213@naver.com>
2022-09-20 17:41:35 +0900

    product3

---
+++ src/main/java/com/kh/demo/web/form/SaveForm.java
@@ -0,0 +1,28 @@
+package com.kh.demo.web.form;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+@Data
+public class SaveForm {
+  @NotBlank
+  private String pname;     //  PNAME	VARCHAR2(30 BYTE)
+  @NotNull
+  @Positive
+  @Max(9999999999L)
+  private Long quantity;    //  QUANTITY	NUMBER(10,0)
+  @NotNull
+  @Positive
+  @Max(9999999999L)
+  private Long price;       //  PRICE	NUMBER(10,0)
+  private MultipartFile file; //상품설명 첨부파일 필드
+  private List<MultipartFile> files; //상품 이지지 첨부(여러건)
+
+}
+
